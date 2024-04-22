18497f6e1ca37f7faf4636c5eb2cb267dfd0a34a
OscarUD <o.perez@opendeusto.es>
2024-04-22 11:50:01 +0200

    region test, tipo test unitarios

--- src/test/java/com/conection/RegionTest.java
+++ src/test/java/com/conection/RegionTest.java
@@ -10,27 +10,56 @@ import com.conection.entities.Region;
 @SpringBootTest
 public class RegionTest {
 
-    private Region region;
+    private Region region1;
+    private Region region2;
 
     @Before
     public void setUp() {
-        region = new Region();
+        region1 = new Region();
+        region2 = new Region(1, "Test Region");
+    }
+
+    @Test 
+    public void testNoArgsConts() {
+        Region region11 = new Region();
+        
+        Assert.assertEquals(region1.getId(), region11.getId());
+        Assert.assertEquals(region1.getRegion(), region11.getRegion());
     }
 
     @Test
-    public void testGetId() {
+    public void testArgsContsTrue() {
+        Region region22 = new Region(1, "Test Region");
+        
+        Assert.assertEquals(region2.getId(), region22.getId());
+        Assert.assertEquals(region2.getRegion(), region22.getRegion());
+    }
+
+    @Test 
+    public void testGetID() {
         int expectedId = 1;
-        region.setId(expectedId);
-        int actualId = region.getId();
+        Assert.assertEquals(expectedId, region2.getId());
+    }
+
+    @Test
+    public void testSetId() {
+        int expectedId = 1;
+        region2.setId(expectedId);
+        int actualId = region2.getId();
         Assert.assertEquals(expectedId, actualId);
     }
 
     @Test
     public void testGetRegion() {
         String expectedRegion = "Test Region";
-        region.setRegion(expectedRegion);
-        String actualRegion = region.getRegion();
-        Assert.assertEquals(expectedRegion, actualRegion);
+        Assert.assertEquals(expectedRegion, region2.getRegion());
+    }
+
+    @Test
+    public void testSetRegion() {
+        String expectedRegion = "Test Region";
+        region2.setRegion(expectedRegion);
+        Assert.assertEquals(expectedRegion, region2.getRegion());
     }
     
 }
