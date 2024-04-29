d1ae61ef7a16212b912809939a609ec5b80cd275
OscarUD <o.perez@opendeusto.es>
2024-04-29 12:13:38 +0200

    usuario

---
+++ src/main/java/com/conection/entities/Usuario.java
@@ -0,0 +1,33 @@
+package com.conection.entities;
+
+
+import javax.jdo.annotations.PersistenceCapable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "Tipos")
+@PersistenceCapable
+public class Usuario {
+
+    @Id
+    private int ID;
+
+    private String Correo;
+    private String Contra;
+    private int Nivel;
+
+    
+}
